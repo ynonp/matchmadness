387d4d13e793a473f1f7e2b6f81828d0be081a5e
ynonp <ynonperek@gmail.com>
2024-05-18 20:01:54 +0300

    add more cards

--- src/app/server.ts
+++ src/app/server.ts
@@ -8,5 +8,8 @@ export async function getCards() {
     { id: 4, playCount: 0, text: "JUICE", image: "/img/juice.webp" },
     { id: 5, playCount: 0, text: "LEMON", image: "/img/lemon.jpg" },
     { id: 6, playCount: 0, text: "Milkshake", image: "/img/milkshake.jpg" },
+    { id: 7, playCount: 0, text: "MILK", image: "/img/milk.jpg"},
+    { id: 8, playCount: 0, text: "SOUP", image: "/img/soup.jpg"},
+    { id: 9, playCount: 0, text: "VEGETABLES", image: "/img/vegetables.jpg"},
   ])
 }
